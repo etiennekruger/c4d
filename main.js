fa44c80b2f39ddc56a2f634b8c5caaf283a2339a
Etienne Kruger <el@Osiris.(none)>
2012-08-05 10:49:02 +0200

    Changed to dark cloudmade theme.

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var hull = new L.LatLng(-28.6, 23.2);
 function initmap() {
     map = new L.Map('map');
-    var mapurl="http://{S}tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/999/256/{Z}/{X}/{Y}.png"
-    //var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var mapurl="http://tile.cloudmade.com/57139bdd766242dbbd46f1b4c1394420/999/256/{z}/{x}/{y}.png"
     var mapattrib='Map data: cloudmade';
-    var osm = new L.TileLayer(osmUrl,{minZoom:2,maxZoom:18,attribution:mapattrib});
+    var osm = new L.TileLayer(mapurl,{minZoom:2,maxZoom:18,attribution:mapattrib});
     
     map.setView(hull,6);
     map.addLayer(osm);
