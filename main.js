0e7a317941c72f07b13ef201e209d8dfd9ee21fc
Etienne Kruger <el@Osiris.(none)>
2012-08-05 11:26:56 +0200

    Updated JS.

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 var hull = new L.LatLng(-28.6, 23.2);
 function initmap() {
+    layers = {};
     map = new L.Map('map');
-    var mapurl="http://tile.cloudmade.com/57139bdd766242dbbd46f1b4c1394420/999/256/{z}/{x}/{y}.png"
+    var hull = new L.LatLng(-28.6, 23.2);
+    var mapurl='http://tile.cloudmade.com/57139bdd766242dbbd46f1b4c1394420/999/256/{z}/{x}/{y}.png'
     var mapattrib='Map data: cloudmade';
     var osm = new L.TileLayer(mapurl,{minZoom:2,maxZoom:18,attribution:mapattrib});
-    
+   
     map.setView(hull,6);
     map.addLayer(osm);
-    
-    $.getJSON('test.json', success=function(data) {
-	L.geoJson(data, {
-	    style: function (feature) {
-		return {color: "blue"};
-	    },
-	    onEachFeature: function (feature, layer) {
-		layer.bindPopup(feature.properties.WARDWINNER);
-	    }
-	}).addTo(map);
-    });
 }
+
+function popup(feature, layer) {
+    return feature.properties.WARDWINNER;
+}
+
+function layerchange() {
+    var src = $(this).data('src');
+    var layer = layers[src];
+    console.log(layers);
+    console.log(layer);
+    if (typeof(layer) == 'undefined') {
+	console.log('adding layer');
+	console.log(src);
+	$.getJSON(src, success=function(data) {
+	    layer = L.geoJson(data, {
+		style: function (feature) {
+		    return {color: 'blue'};
+		},
+		onEachFeature: function (feature, layer) {
+		    layer.bindPopup(popup(feature, layer));
+		}
+	    }).addTo(map);
+	    layers[src] = layer;
+	    console.log(layers);
+	});
+    } else {
+	console.log('removing layer');
+	map.removeLayer(layer);
+	console.log(layers);
+    }
+}
+
 initmap();
+layerchange('test.json');
